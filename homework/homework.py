c8c7386f3564663e4711161d96af73c7a04a21f1
Keavy-c <100011260+Keavy-c@users.noreply.github.com>
2022-07-03 21:53:45 +0800

    update

---
+++ homework/homework.py
@@ -0,0 +1,8 @@
+num = input("请输入一个数字：")
+all_infos = []
+for i in range(num):
+    a,b = input().split()
+    all_infos.append(a)
+    all_infos.append(b)
+    sum = int(tmp[0]) + int(tmp[1])
+print(sum)
